f54739491c31b8c1943b45da81f216adf118594c
dharshanspn <86490616+dharshanspn@users.noreply.github.com>
2024-11-01 23:40:39 +0530

    Update script.py

--- script.py
+++ script.py
@@ -11,7 +11,7 @@ import multiprocessing
 
 # Account credentials (you can add more accounts here)
 accounts = [
-    {"username": "anwarchegg05@gmail.com", "password": "Author@05", "user_bot_chatID": '1155462778', "account_name": "Anwar", "user_bot_token" : "8131045025:AAE9_BMb5i2pk479mubtilbSIUilPA25jWM"},
+    {"username": "anwarchegg05@gmail.com", "password": "Chegg@050402", "user_bot_chatID": '1155462778', "account_name": "Anwar", "user_bot_token" : "8131045025:AAE9_BMb5i2pk479mubtilbSIUilPA25jWM"},
     {"username": "sunitha05chegg@gmail.com", "password": "Chegg@050402", "user_bot_chatID": '1155462778', "account_name": "Sunitha", "user_bot_token" : "8131045025:AAE9_BMb5i2pk479mubtilbSIUilPA25jWM"},
     {"username": "rahulchegg5@gmail.com", "password": "Author@05", "user_bot_chatID": '1155462778', "account_name": "Rahul", "user_bot_token" : "8131045025:AAE9_BMb5i2pk479mubtilbSIUilPA25jWM"},
     {"username": "nirupeshcs@icloud.com", "password": "#Hitlin3945", "user_bot_chatID": '1155462778', "account_name": "Nirupesh", "user_bot_token" : "8131045025:AAE9_BMb5i2pk479mubtilbSIUilPA25jWM"},
@@ -43,8 +43,8 @@ def refresh_account(account):
     flag_login = True
     while flag_login:
         flag_login = login_to_chegg(username, password, driver)
-    #login_texts = f"Login on {account_name}"
-    #telegram_bot_sendtext(login_texts,user_bot_token,user_bot_chatID)    
+    login_texts = f"Login on {account_name}"
+    telegram_bot_sendtext(login_texts,user_bot_token,user_bot_chatID)    
 
 
     # Start refreshing for the account
